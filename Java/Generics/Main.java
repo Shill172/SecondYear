b03c57a7dfa63f3bc9e8b2f3cdb975f994875082
Scott <scotthill006@gmail.com>
2025-10-01 19:32:56 +0100

    Small consolidation of Generics

---
+++ Java/Generics/Main.java
@@ -0,0 +1,11 @@
+public class Main {
+    public static void main(String[] args) {
+
+        Pair<String, String> strPair = new Pair<>("Hello", "World");
+        Pair<Integer, Integer> intPair = new Pair<>(123, 456);
+
+        System.out.println(strPair);
+        System.out.println(intPair);
+
+    }
+}
